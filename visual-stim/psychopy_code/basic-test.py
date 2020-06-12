bdabee56e41f94d6cf2df9ee6f1b4743de0cdf25
yzerlaut <yann.zerlaut@gmail.com>
2020-06-12 15:10:22 +0200

    update basic test to dynamic

--- visual-stim/psychopy_code/basic-test.py
+++ visual-stim/psychopy_code/basic-test.py
@@ -1,16 +1,23 @@
-from psychopy import visual, core  # import some libraries from PsychoPy
+from psychopy import visual, core, event #import some libraries from PsychoPy
 
 #create a window
-mywin = visual.Window([800,600], monitor="testMonitor", units="deg")
+mywin = visual.Window([800,600],monitor="testMonitor", units="deg")
 
 #create some stimuli
-grating = visual.GratingStim(win=mywin, mask="circle", size=3, pos=[-4,0], sf=3)
-fixation = visual.GratingStim(win=mywin, size=0.5, pos=[0,0], sf=0, rgb=-1)
+grating = visual.GratingStim(win=mywin, mask='circle', size=3, pos=[-4,0], sf=3)
+fixation = visual.GratingStim(win=mywin, size=0.2, pos=[0,0], sf=0, rgb=-1)
 
 #draw the stimuli and update the window
-grating.draw()
-fixation.draw()
-mywin.update()
+while True: #this creates a never-ending loop
+    grating.setPhase(0.01, '+')#advance phase by 0.05 of a cycle
+    grating.draw()
+    fixation.draw()
+    mywin.flip()
 
-#pause, so you get a chance to see it!
-core.wait(5.0)
+    if len(event.getKeys())>0:
+        break
+    event.clearEvents()
+
+#cleanup
+mywin.close()
+core.quit()
