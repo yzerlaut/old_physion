20745199b9d43e327d15a1c7089ff76f748ca095
yzerlaut <yann.zerlaut@gmail.com>
2020-06-16 14:51:06 +0200

    adding init files

--- visual-stim/gui/main.py
+++ visual-stim/gui/main.py
@@ -1,11 +1,19 @@
-from PyQt5 import QtGui, QtWidgets, QtCore
 import sys
+from PyQt5.QtWidgets import QApplication, QWidget
+
+
+def main():
+
+    app = QApplication(sys.argv)
+
+    w = QWidget()
+    w.resize(600, 300)
+    w.move(300, 300)
+    w.setWindowTitle('Visual Stimulation Program')
+    w.show()
+
+    sys.exit(app.exec_())
 
 
 if __name__ == '__main__':
-    import time
-    app = QtWidgets.QApplication(sys.argv)
-    main = Window(app)
-    # main.show()
-    sys.exit(app.exec_())     
-
+    main()
--- visual-stim/psychopy_code/static-stimuli.py
+++ visual-stim/psychopy_code/static-stimuli.py
@@ -12,7 +12,7 @@ fixation = visual.GratingStim(win=mywin, size=0.2, pos=[0,0], sf=0, rgb=-1)
 #draw the stimuli and update the window
 i=0
 while True: #this creates a never-ending loop
-    grating.setPhase(0.01, '+')#advance phase by 0.05 of a cycle
+    # grating.setPhase(0.01, '+')#advance phase by 0.05 of a cycle
     grating.draw()
     fixation.draw()
     mywin.flip()
