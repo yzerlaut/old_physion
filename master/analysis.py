ca758c9941bfb7ecd5338727bc5f5a3a195fb7db
yzerlaut <yann.zerlaut@gmail.com>
2020-09-16 18:36:41 +0200

    redrafted analysis gui

--- master/analysis.py
+++ master/analysis.py
@@ -1,89 +1,66 @@
 import sys, time, tempfile, os, pathlib, json, subprocess
-import threading # for the camera stream
 import numpy as np
 from PyQt5 import QtGui, QtWidgets, QtCore
+import pyqtgraph as pg
 
 sys.path.append(str(pathlib.Path(__file__).resolve().parents[1]))
-from assembling.saving import *
-from assembling.analysis import quick_data_view, analyze_data, last_datafile
+from assembling.saving import day_folder, generate_filename_path, save_dict, load_dict
 
+
+## NASTY workaround to the error:
+# ** OMP: Error #15: Initializing libiomp5md.dll, but found libiomp5md.dll already initialized. **
 
 class MasterWindow(QtWidgets.QMainWindow):
     
     def __init__(self, app,
-                 parent=None,
-                 button_length = 110):
+                 saturation=100):
         
-        super(MasterWindow, self).__init__(parent)
+        super(MasterWindow, self).__init__()
+
         
-        self.data_folder = get_data_folder()
+        # adding a "quit" keyboard shortcut
+        self.quitSc = QtWidgets.QShortcut(QtGui.QKeySequence('Q'), self) # or 'Ctrl+Q'
+        self.quitSc.activated.connect(self.quit)
+
         
         self.setWindowTitle('Analysis Program -- Physiology of Visual Circuits')
-        self.setGeometry(50, 50, 500, 130)
+        pg.setConfigOptions(imageAxisOrder='row-major')
+        # self.setGeometry(0,0,1600,1200)
+        self.showFullScreen()
+        self.setStyleSheet("QMainWindow {background: 'black';}")
 
-        # buttons and functions
-        LABELS = ["l) Load data", "v) View data", "r) Run analysis", "q) Quit"]
-        FUNCTIONS = [self.load_data, self.view_data, self.run, self.quit]
         
-        mainMenu = self.menuBar()
-        self.fileMenu = mainMenu.addMenu('')
+        self.cwidget = QtGui.QWidget(self)
+        self.setCentralWidget(self.cwidget)
+        self.l0 = QtGui.QGridLayout()
+        self.cwidget.setLayout(self.l0)
+        self.win = pg.GraphicsLayoutWidget()
+        self.win.move(50,50)
+        self.win.resize(300,300)
+        self.l0.addWidget(self.win,1,3,37,15)
 
-        for func, label, shift in zip(FUNCTIONS, LABELS,\
-                                      button_length*np.arange(len(LABELS))):
-            btn = QtWidgets.QPushButton(label, self)
-            btn.clicked.connect(func)
-            btn.setMinimumWidth(button_length)
-            btn.move(shift+30, 20)
-            action = QtWidgets.QAction(label, self)
-            action.setShortcut(label.split(')')[0])
-            action.triggered.connect(func)
-            self.fileMenu.addAction(action)
-
-        self.dfl = QtWidgets.QLabel('Data-Folder (root): "%s"' % str(self.data_folder), self)
-        self.dfl.setMinimumWidth(300)
-        self.dfl.move(30, 70)
-        dfb = QtWidgets.QPushButton('Set folder', self)
-        dfb.clicked.connect(self.choose_data_folder)
-        dfb.move(350, 70)
-            
-        self.statusBar = QtWidgets.QStatusBar()
-        self.setStatusBar(self.statusBar)
-        self.statusBar.showMessage('ready for analysis')
         
+        self.p0 = self.win.addViewBox(lockAspect=True,row=0,col=0,invertY=True,border=[100,100,100])
+        self.p0.setAspectLocked()
+        self.p0img = pg.ImageItem(None)
+        self.p0.addItem(self.p0img)
+        self.pROI = self.win.addViewBox(lockAspect=True,row=0,col=1,invertY=True, border=[100,100,100])
+        self.pROI.setAspectLocked()
+        self.pROIimg = pg.ImageItem(None)
+        self.pROI.addItem(self.pROIimg)
+
+        self.pROI.show()
+        self.win.show()
         self.show()
 
-    def run(self):
-        self.statusBar.showMessage('Analyzing last recording [...]')
-        data, fig1 = quick_data_view(last_datafile(tempfile.gettempdir()), realign=True)
-        _, fig2 = analyze_data(data=data)
-        fig1.show()
-        fig2.show()
 
-    def choose_data_folder(self):
-        fd = str(QtWidgets.QFileDialog.getExistingDirectory(self,
-                                                            "Select Root Data Folder", self.data_folder))
-        if os.path.isdir(fd):
-            self.data_folder = fd
-            set_data_folder(fd)
-            self.dfl.setText('Data-Folder (root): "%s"' % str(self.data_folder))
-        else:
-            self.statusBar.showMessage('Invalid folder -> folder unchanged')
-        
-    def view_data(self):
-        _, fig = quick_data_view(last_datafile(self.data_folder))
-        fig.show()
-        
-    def load_data(self):
-        pass
-    
-    def run(self):
-        pass
-    
     def quit(self):
+        try:
+            self.camera.cam.stop()
+        except Exception:
+            pass
         sys.exit()
 
-if __name__ == '__main__':
-    
-    app = QtWidgets.QApplication(sys.argv)
-    main = MasterWindow(app)
-    sys.exit(app.exec_())
+app = QtWidgets.QApplication(sys.argv)
+main = MasterWindow(app)
+sys.exit(app.exec_())
