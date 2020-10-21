a5d2f700634afa66d7552c9cd3ca63889d86abb7
Marcel <marcelbvv@gmail.com>
2020-10-21 21:49:15 +0200

    Changes to RaspberryPi __main__.py

--- hardware_control/RaspberryPi/main.py
+++ hardware_control/RaspberryPi/main.py
@@ -1,4 +1,4 @@
-import sys
+import sys, time
 
 import RPi.GPIO as GPIO
 
@@ -9,25 +9,57 @@ from zaber_stage import ZaberStageControl
 class StageController:
 
     def __init__(self,
-                 stage_controller_args={}):
+                 stage_controller_args={},
+                 triggers = [],
+                 modes = [],
+                 actions = []):
 
-        stage_controller = ZaberStageControl(**stage_controller_args)
-
+        self.stage_controller = ZaberStageControl(**stage_controller_args)
+        self.channels = channels
+        self.modes = modes
+        self.actions = actions
+        
         GPIO.setwarnings(False) # Ignore warning for now
         GPIO.setmode(GPIO.BCM) # Use physical pin numbering
-        GPIO.setup(11, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-        GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-        GPIO.add_event_detect(11, GPIO.RISING)
-        GPIO.add_event_callback(11, stage_controller.stage_move)
-        GPIO.add_event_detect(12, GPIO.RISING)
-        GPIO.add_event_callback(12, stage_controller.start_stop)
-
+        for channel, mode, action in zip(self.channels, self.mode, self.actions):
+            initiate_trigger(self, channel, mode, action)
+        
+        
+    def initiate_trigger(self, channel, mode, action):
+        if mode == 'in':
+            GPIO.setup(channel, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        elif mode == 'out':
+            GPIO.setup(channel, GPIO.OUT)
+        else:
+            print('Unable to generate IO function')
+        
+        if mode == 'Rising':
+            GPIO.add_event_detect(channel, GPIO.RISING)
+        elif mode == 'Falling':
+            GPIO.add_event_detect(channel, GPIO.FALLING)
+        elif mode == 'Both':
+            GPIO.add_event_detect(channel, GPIO.BOTH)
+        else:
+            print('Mode selected for channel {} incorrect.'.format(channel))
+            print('Must be Rising, Falling or Both')
+        
+        if action == 'move stage':
+            GPIO.add_event_callback(channel, self.stage_controller.stage_move)
+        elif action == 'start_stop':
+            GPIO.add_event_callback(channel, self.stage_controller.start_stop)  
+    
+    
+    
+    def stop_trigger(self, channel):
+        GPIO.cleanup(channel)
+    
+    
+    def stop_all(self):
+        GPIO.cleanup()
+    
 
 if __name__=='__main__':
 
-    print('*** Starting whisker stimulation *** \n')
-    print('[1] Setting up the controller')
-
-    time.sleep(2)
-    
+    print('- Setting up the controller')
     pi = StageController()
+    time.sleep(2)
