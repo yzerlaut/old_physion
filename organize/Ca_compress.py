30130b81c63be5c934ffa30f4c1d6a353e4b1aff
yzerlaut <yann.zerlaut@gmail.com>
2020-11-09 18:53:53 +0100

    update on Ca-imaging compression to mp4

--- organize/Ca_compress.py
+++ organize/Ca_compress.py
@@ -1,122 +1,113 @@
 import numpy as np
 import skvideo.io
 import imageio
-import os, pathlib
+import sys, os, pathlib
 import matplotlib.pylab as plt
 from scipy.ndimage import gaussian_filter
 
 sys.path.append(str(pathlib.Path(__file__).resolve().parents[1]))
+from hardware_control.Bruker.xml_parser import bruker_xml_parser
 from assembling.binary import BinaryFile
+from assembling.saving import get_files_with_extension, list_dayfolder, check_datafolder
+from organize.Ca_process import list_TSeries_folder
 
-def compress_FluorescenceMovie(binaryFile,
+def compress_FluorescenceMovie(folder, xml,
+                               plane=0,
                                smoothing=0, # either 2, or (1, 4, 5)
                                subsampling=1,
-                               extension='.npz', # '.avi', or '.mp4', ...
-                               tool='numpy',
+                               extension='.mp4', # '.avi', or '.mp4', ...
+                               tool='skvideo',
                                Nframe_per_file=1000,
                                max_file=int(1e6),
                                verbose=False):
 
 
+    
+    Lx, Ly = int(xml['settings']['pixelsPerLine']), int(xml['settings']['linesPerFrame'])
+    
+    binaryFile = os.path.join(folder, 'suite2p', 'plane%i' % plane, 'data.bin')
+    filename = binaryFile.replace('data.bin', 'Fluorescence'+extension)
+    
     data = BinaryFile(Ly=Ly, Lx=Lx, read_filename=binaryFile)
+    imax = data.shape[0]
 
-    """
-    # create directory if not existing
-    directory = os.path.join(datafolder, 'FaceCamera-compressed')
-    pathlib.Path(directory).mkdir(parents=True, exist_ok=True)
+    def rescale_function(im):
+        return (im+600.)/4600.*255.
+    
+    print('converting binary file "%s" to mp4 [...]', binaryFile)
     
     # ------------------------------------------------------------
     # building custom compression functions depending on the tool
-
-    if tool=='numpy':
-        
-        extension='.npz' # forcing the extension in that case
-        def compress_func(X, filename):
-            np.savez_compressed(filename+extension, X)
-            
-    elif tool=='skvideo':
-
-        if extension not in ['.avi', '.mp4']:
-            print('forcing "mp4" extension')
-            extension = '.mp4' 
-        def compress_func(X, filename):
-            writer = skvideo.io.FFmpegWriter(filename+extension)
-            for i in range(X.shape[0]):
-                writer.writeFrame(X[i,:,:])
-            writer.close()
-            
-    elif tool=='imageio':
-
-        if extension not in ['.avi', '.mp4']:
-            print('forcing "mp4" extension')
-            extension = '.mp4' 
-        def compress_func(X, filename):
-            imageio.mimwrite(filename+extension, X)
-
-    # ------------------------------------------------
-    # Now looping over frames to build the compression
-
-    FILES = sorted(os.listdir(os.path.join(datafolder, 'FaceCamera-imgs')))
-    print(FILES)
-    if len(FILES)>0:
-        X, i0, i, file_count = [], 0, 0, 0
-        for i, fn in enumerate(FILES):
-            x = np.load(os.path.join(datafolder, 'FaceCamera-imgs', fn))
+    if tool=='skvideo':
+        writer = skvideo.io.FFmpegWriter(filename)
+        i = -1
+        while i<imax:
+            i, im = data.read()
             if smoothing!=0:
-                x = gaussian_filter(x, smoothing)
-            X.append(x)
+                im = gaussian_filter(im, smoothing)
+            writer.writeFrame(rescale_function(im))
+        writer.close()
+        print('[ok] ---> binary file converted to: %s' % filename)
+    elif tool=='imageio':
+        imageio.mimwrite(filename, X)
 
-            if ((i+1)%Nframe_per_file)==0:
-                # we save
-                filename = 'imgs-%i-%i' % (i0, i)
-                compress_func(np.array(X), os.path.join(directory, filename))
-                # and we reinit
-                X, i0 = [], i+1
-                if verbose:
-                    print('wrote: ', filename)
+    
+    # # ------------------------------------------------
+    # # Now looping over frames to build the compression
 
-                file_count +=1
+    # FILES = sorted(os.listdir(os.path.join(datafolder, 'FaceCamera-imgs')))
+    # print(FILES)
+    # if len(FILES)>0:
+    #     X, i0, i, file_count = [], 0, 0, 0
+    #     for i, fn in enumerate(FILES):
+    #         x = np.load(os.path.join(datafolder, 'FaceCamera-imgs', fn))
+    #         if smoothing!=0:
+    #             x = gaussian_filter(x, smoothing)
+    #         X.append(x)
 
-            if file_count>=max_file:
-                break
+    #         if ((i+1)%Nframe_per_file)==0:
+    #             # we save
+    #             filename = 'imgs-%i-%i' % (i0, i)
+    #             compress_func(np.array(X), os.path.join(directory, filename))
+    #             # and we reinit
+    #             X, i0 = [], i+1
+    #             if verbose:
+    #                 print('wrote: ', filename)
 
-        # saving the last frames
-        if file_count<max_file:
-            filename = 'imgs-%i-%i' % (i0, i)
-            compress_func(np.array(X), os.path.join(directory, filename))
-            if verbose:
-                print('wrote: ', filename)
+    #             file_count +=1
 
-        # saving compression metadata 
-        compression_metadata = {'tool':tool,
-                                'subsampling':subsampling,
-                                'smoothing':smoothing,
-                                'extension':extension,
-                                'Nframe_per_file':Nframe_per_file,
-                                'max_file':max_file}
-        np.save(os.path.join(directory, 'compression-metadata.npy'), compression_metadata)
+    #         if file_count>=max_file:
+    #             break
 
-    """
-def compress_datafolder(args):
+    #     # saving the last frames
+    #     if file_count<max_file:
+    #         filename = 'imgs-%i-%i' % (i0, i)
+    #         compress_func(np.array(X), os.path.join(directory, filename))
+    #         if verbose:
+    #             print('wrote: ', filename)
 
-    compress_FaceCamera(args.datafolder,
-                        extension=args.extension,
-                        tool=args.tool,
-                        smoothing=args.smoothing,
-                        verbose=args.verbose,
-                        Nframe_per_file=args.Nframe_per_file,
-                        max_file=args.max_file)
+    #     # saving compression metadata 
+    #     compression_metadata = {'tool':tool,
+    #                             'subsampling':subsampling,
+    #                             'smoothing':smoothing,
+    #                             'extension':extension,
+    #                             'Nframe_per_file':Nframe_per_file,
+    #                             'max_file':max_file}
+    #     np.save(os.path.join(directory, 'compression-metadata.npy'), compression_metadata)
+
     
                 
 if __name__=='__main__':
 
-    fn = '/home/yann/DATA/2020_11_03/TSeries-28102020-231-00-031/'
+    # fn = '/home/yann/DATA/2020_11_03/TSeries-28102020-231-00-031/'
+    folder = '/home/yann/DATA/2020_11_03/'
 
-    xml = bruker_xml_parser(get_files_with_extension(fn, extension='.xml')[0])
-    Lx, Ly = xml['settings']['pixelsPerLine'], xml['settings']['linesPerFrame']
-    
-
-    compress_FluorescenceMovie(Ly, Lx, os.path.join(fn, 'suite2p', 'plane0', 'data.bin'))
+    for bdf in list_TSeries_folder(folder):
+        fn = get_files_with_extension(bdf, extension='.xml')[0]
+        xml = bruker_xml_parser(fn)
+        binaryFile = os.path.join(bdf, 'suite2p', 'plane0', 'data.bin')
+        if len(xml['Ch1']['absoluteTime'])>10 and os.path.isfile(binaryFile):
+            compress_FluorescenceMovie(bdf, xml)
     
     # import argparse
 
