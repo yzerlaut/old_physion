ceed603367c1dfa09d502b7cb5569335337d2538
ZERLAUT Yann <yann.zerlaut@icm-institute.org>
2020-12-22 15:06:00 +0100

    up

--- Ca_imaging/process_xml.py
+++ Ca_imaging/process_xml.py
@@ -4,11 +4,19 @@ sys.path.append(str(pathlib.Path(__file__).resolve().parents[1]))
 from hardware_control.Bruker.xml_parser import bruker_xml_parser
 from assembling.saving import from_folder_to_datetime, check_datafolder, get_files_with_given_exts
 
-folder = '/home/yann/DATA/2020.09.25/M_1/TSeries-25092020-200-00-001'
+# folder = '/home/yann/DATA/2020.09.25/M_1/TSeries-25092020-200-00-001'
 
-fn = get_files_with_given_exts(dir=folder, EXTS=['xml'])[0]
+# fn = get_files_with_given_exts(dir=folder, EXTS=['xml'])[0]
 
-print(bruker_xml_parser(fn))
+# print(bruker_xml_parser(fn))
 
 
     
+if __name__=='__main__':
+    
+    folder = '/media/yann/Yann/2020_11_10/TSeries-11102020-1605-016'
+    
+    xml_file = os.path.join(folder, os.path.join(folder.split('/')[-1],'xml'))
+    
+    print(xml_file)
+    # print(fn.split(os.path.sep))
