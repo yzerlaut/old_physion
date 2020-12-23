55ec4ce832d0436408292c3d815fc070d6e9b80c
ZERLAUT Yann <yann.zerlaut@icm-institute.org>
2020-12-23 18:42:50 +0100

    rm script

--- script.sh
+++
@@ -1,7 +0,0 @@
-run_preprocessing() {
-    python Ca_imaging/process_xml.py $1
-    suite2p --db $1/db.npy --ops $1/ops.npy
-}
-
-run_preprocessing /home/yann/DATA/2020_12_09/TSeries-12092020-1112-003
-
