e30d3955cb1c157e27c9c8b041c57542c9a8f679
ZERLAUT Yann <yann.zerlaut@icm-institute.org>
2021-02-12 13:10:57 +0100

    up

--- assembling/realign_from_photodiode.py
+++ assembling/realign_from_photodiode.py
@@ -1,5 +1,7 @@
 import numpy as np
 import os
+from scipy.signal import argrelextrema
+from scipy.ndimage.filters import gaussian_filter1d
 
 def realign_from_photodiode(signal, metadata,
                             debug=False, verbose=True):
@@ -16,33 +18,42 @@ def realign_from_photodiode(signal, metadata,
     
     tlim, tnew = [0, t[-1]], 0
 
+    pre_window = np.min([metadata['presentation-interstim-period'], metadata['presentation-prestim-period']])
     t0 = metadata['time_start'][0]
-    length = metadata['presentation-duration']+metadata['presentation-interstim-period']
-    npulses = int(metadata['presentation-duration'])
+    # length = metadata['presentation-duration']+metadata['presentation-interstim-period']
+    # npulses = int(metadata['presentation-duration'])
     metadata['time_start_realigned'] = []
     Nepisodes = np.sum(metadata['time_start']<tlim[1])
-    for i in range(Nepisodes):
-        cond = (t>=t0-.3) & (t<=t0+length)
-        try:
-            tnew, integral, threshold = find_onset_time(t[cond]-t0,
-                                                        signal[cond], npulses)
-            if debug and ((i<3) or (i>Nepisodes-3)):
-                fig, ax = plt.subplots()
 
-                ax.plot(t[cond], normalize_signal(signal[cond])[0], label='photodiode-signal')
-                norm_integral, norm, xmin = normalize_signal(integral)
-                ax.plot(t[cond], norm_integral, label='integral')
-                ax.plot(t[cond], integral*0+norm*(threshold-xmin), label='threshold')
-                ax.plot([t0,t0,], [0,1], 'k:', label='onset')
+    H, bins = np.histogram(signal, bins=200)
+    baseline = .5*(bins[np.argmax(H)]+bins[np.argmax(H)+1])
+    high_level = np.max(signal)
+    print(metadata['time_stop'][-1])
+    i=0
+    while (i<Nepisodes) and (t0<t[-1]):
+        cond = (t>=t0-pre_window) & (t<=t0+metadata['time_duration'][i]+metadata['presentation-interstim-period'])
+        try:
+            tshift, integral, threshold = find_onset_time(t[cond]-t0, signal[cond],
+                                                          baseline=baseline, high_level=high_level)
+            if debug and ((i<5) or (i>Nepisodes-30)):
+                fig, ax = plt.subplots()
+                ax.plot(t[cond], integral, label='integral')
+                ax.plot(t[cond], integral*0+threshold, label='threshold')
+                ax.plot((t0+tshift)*np.ones(2), ax.get_ylim(), 'k:', label='onset')
+                ax.plot((t0+tshift+metadata['time_duration'][i])*np.ones(2), ax.get_ylim(), 'k:', label='offset')
+                ax.plot(t[cond], normalize_signal(signal[cond])[0]*.8*np.diff(ax.get_ylim())[0],
+                        label='photodiode-signal', lw=0.5, alpha=.3)
                 plt.xlabel('time (s)')
                 plt.ylabel('norm. signals')
                 ax.legend(frameon=False)
                 plt.show()
-        except Exception:
+        except BaseException as be:
+            print(be)
+            print(i, Nepisodes, metadata['time_duration'][i])
             success = False # one exception is enough to make it fail
-        t0+=tnew
-        metadata['time_start_realigned'].append(t0)
-        t0+=length
+        metadata['time_start_realigned'].append(t0+tshift)
+        t0=t0+tshift+metadata['time_duration'][i]+metadata['presentation-interstim-period']
+        i+=1
 
     if verbose:
         if success:
@@ -52,25 +63,28 @@ def realign_from_photodiode(signal, metadata,
     if success:
         metadata['time_start_realigned'] = np.array(metadata['time_start_realigned'])
         metadata['time_stop_realigned'] = metadata['time_start_realigned']+\
-            metadata['presentation-duration']
+            metadata['time_duration'][:len(metadata['time_start_realigned'])]
     else:
         metadata['time_start_realigned'] = np.array([])
         metadata['time_stop_realigned'] = np.array([])
+    print(metadata['time_start'], metadata['time_start_realigned'])
     return success, metadata
 
 
-def find_onset_time(t, photodiode_signal, npulses,
-                    time_for_threshold=10e-3):
+def find_onset_time(t, photodiode_signal,
+                    time_for_threshold=20e-3, baseline=0, high_level=1):
     """
     the threshold of integral increase corresponds to spending X-ms at half the maximum
     """
-    H, bins = np.histogram(photodiode_signal, bins=100)
-    baseline = bins[np.argmax(H)]
-
+    # H, bins = np.histogram(photodiode_signal, bins=50)
     integral = np.cumsum(photodiode_signal-baseline)*(t[1]-t[0])
 
+    # threshold = time_for_threshold*bins[hist_extrema[-1]]
     threshold = time_for_threshold*np.max(photodiode_signal)
-    t0 = t[np.argwhere(integral>threshold)[0][0]]
+    # threshold = time_for_threshold*high_level
+    cond = np.argwhere(integral>threshold)
+    t0 = t[cond[0][0]]
+
     return t0-time_for_threshold, integral, threshold
 
 def normalize_signal(x):
@@ -92,7 +106,7 @@ if __name__=='__main__':
     data = np.load(os.path.join(args.datafolder, 'NIdaq.npy'), allow_pickle=True).item()['analog'][0]
     metadata = np.load(os.path.join(args.datafolder, 'metadata.npy'), allow_pickle=True).item()
     VisualStim = np.load(os.path.join(args.datafolder, 'visual-stim.npy'), allow_pickle=True).item()
-    for key in ['time_start', 'time_stop']:
+    for key in ['time_start', 'time_stop', 'time_duration']:
         metadata[key] = VisualStim[key]
 
 
