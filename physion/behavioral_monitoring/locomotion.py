2764850ebd922c987f90c9c757228be826b4036b
Yann Zerlaut <yann.zerlaut@gmail.com>
2021-04-23 13:33:16 +0200

    update locomotion

--- physion/behavioral_monitoring/locomotion.py
+++ physion/behavioral_monitoring/locomotion.py
@@ -60,15 +60,14 @@ if __name__=='__main__':
 
     import argparse
     # First a nice documentation 
-    parser=argparse.ArgumentParser(description="Record data and send signals through a NI daq card",
+    parser=argparse.ArgumentParser(description="""
+    Perform the calibration for the conversion from roto-encoder signal to rotating speed
+    """,
                                    formatter_class=argparse.RawTextHelpFormatter)
-    parser.add_argument('-os', "--output_signal", help="npy file for an array of output signal", default='')
-    parser.add_argument('-Nai', "--Nchannel_analog_rec", help="Number of analog input channels to be recorded ", type=int, default=2)
-    parser.add_argument('-Ndi', "--Nchannel_digital_rec", help="Number of digital input channels to be recorded ", type=int, default=4)
+    parser.add_argument('-Nai', "--Nchannel_analog_rec", help="Number of analog input channels to be recorded ", type=int, default=1)
+    parser.add_argument('-Ndi', "--Nchannel_digital_rec", help="Number of digital input channels to be recorded ", type=int, default=2)
     parser.add_argument('-dt', "--acq_time_step", help="Temporal sampling (in s): 1/acquisition_frequency ", type=float, default=1e-4)
-    parser.add_argument('-T', "--recording_time", help="Length of recording time in (s)", type=float, default=3)
-    parser.add_argument('-f', "--filename", help="filename",type=str, default='data.npy')
-    parser.add_argument('-d', "--device", help="device name", type=str, default='')
+    parser.add_argument('-T', "--recording_time", help="Length of recording time in (s)", type=float, default=10)
     args = parser.parse_args()
 
     if args.device=='':
@@ -76,20 +75,18 @@ if __name__=='__main__':
         
     t_array = np.arange(int(args.recording_time/args.acq_time_step))*args.acq_time_step
     analog_inputs = np.zeros((args.Nchannel_analog_rec,len(t_array)))
-
     analog_outputs = 100*np.array([5e-2*np.sin(2*np.pi*t_array),
                                    2e-2*np.sin(2*np.pi*t_array)])
     
-    print('running rec [...]')
+    print('You have %i s to do 10 rotations of the disk [...]')
     analog_inputs, digital_inputs = stim_and_rec(args.device, t_array, analog_inputs, analog_outputs,
                                                  args.Nchannel_digital_rec)
-    print(digital_inputs[0,:])
 
     A = digital_inputs[0]%2
     B = np.round(digital_inputs[0]/2, 0)
-    print(B)
     
     x = compute_position_from_binary_signals(A, B)
+    
     import matplotlib.pylab as plt
     plt.plot(t_array, x)
     plt.show()
