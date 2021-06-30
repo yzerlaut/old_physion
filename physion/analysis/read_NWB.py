2f5d0abcbede343ba61c058516316b1af57ac18d
Yann Zerlaut <yann.zerlaut@gmail.com>
2021-06-30 09:57:10 +0200

    up

--- physion/analysis/read_NWB.py
+++ physion/analysis/read_NWB.py
@@ -75,7 +75,7 @@ def read(self, filename, verbose=False, with_tlim=True,
             self.Segmentation = self.nwbfile.processing['ophys'].data_interfaces['ImageSegmentation'].plane_segmentations['PlaneSegmentation']
             self.pixel_masks_index = self.Segmentation.columns[0].data[:]
             self.pixel_masks = self.Segmentation.columns[1].data[:]
-            print(np.unique(self.pixel_masks_index))
+
             self.Fluorescence = self.nwbfile.processing['ophys'].data_interfaces['Fluorescence'].roi_response_series['Fluorescence']
             self.Neuropil = self.nwbfile.processing['ophys'].data_interfaces['Neuropil'].roi_response_series['Neuropil']
             self.Deconvolved = self.nwbfile.processing['ophys'].data_interfaces['Deconvolved'].roi_response_series['Deconvolved']
