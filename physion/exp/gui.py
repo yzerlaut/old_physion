b532edd0ec1900287cd1d7928dd803edea89251c
yann <yann.zerlaut@icm-institute.org>
2021-09-27 11:59:58 +0100

    up

--- physion/exp/gui.py
+++ physion/exp/gui.py
@@ -314,7 +314,7 @@ class MainWindow(QtWidgets.QMainWindow):
             np.save(os.path.join(str(self.datafolder.get()), 'visual-stim.npy'), self.stim.experiment)
             print('[ok] Visual-stimulation data saved as "%s"' % os.path.join(str(self.datafolder.get()), 'visual-stim.npy'))
             if 'time_stop' in self.stim.experiment:
-                max_time = int(1.5*self.stim.experiment['time_stop'][-1]) # for security
+                max_time = int(3*self.stim.experiment['time_stop'][-1]) # for security
             else:
                 max_time = 1*60*60 # 1 hour, should be stopped manually
         else:
