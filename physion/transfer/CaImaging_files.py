0cf5472cd6f66a11d8e3a204d0c6e3fe58fed791
Yann Zerlaut <yann.zerlaut@gmail.com>
2021-11-08 13:42:47 +0100

    update loc

--- physion/transfer/CaImaging_files.py
+++ physion/transfer/CaImaging_files.py
@@ -7,17 +7,17 @@ from assembling.saving import get_TSeries_folders
 source_folder = sys.argv[1]
 destination_folder = sys.argv[2]
 
-FILES = ['F', 'Fneu', 'iscell', 'ops', 'spks', 'stat']
+FILES = ['F', 'Fneu', 'iscell', 'ops', 'spks', 'stat', 'redcell']
 if os.path.isdir(source_folder):
     for f in get_TSeries_folders(source_folder):
         if os.path.isdir(os.path.join(f, 'suite2p', 'plane0')):
             print('copying files from "%s"' % f)
             new_folder = os.path.join(destination_folder, f.split(os.path.sep)[-1], 'suite2p', 'plane0')
             pathlib.Path(new_folder).mkdir(parents=True, exist_ok=True)
-            for bfn in ['F', 'Fneu', 'iscell', 'ops', 'spks', 'stat']:
+            for bfn in FILES:
                 old = os.path.join(f, 'suite2p', 'plane0', bfn+'.npy')
                 new = os.path.join(new_folder, bfn+'.npy')
-                shutil.copyfile(old,new)
+                shutil.copyfile(old, new)
             old = os.path.join(f, f.split(os.path.sep)[-1]+'.xml')
             new_folder = os.path.join(destination_folder, f.split(os.path.sep)[-1])
             new = os.path.join(new_folder, f.split(os.path.sep)[-1]+'.xml')
