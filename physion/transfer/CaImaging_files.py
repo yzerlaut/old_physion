f31c4618d3a97876c78160c3b2f489a1adb8dae6
KOUKOULI Fani <fani.koukouli@icm-institute.org>
2021-11-08 14:29:43 +0100

    up"

--- physion/transfer/CaImaging_files.py
+++ physion/transfer/CaImaging_files.py
@@ -11,20 +11,20 @@ FILES = ['F', 'Fneu', 'iscell', 'ops', 'spks', 'stat', 'redcell']
 
 for sf in os.listdir(source_folder1):
     source_folder = os.path.join(source_folder1, sf)
-    print('---', source_folder)
+    print('source_folder ---', source_folder)
     if os.path.isdir(source_folder):
-        print('TSeries folders: ', get_TSeries_folders(source_folder))
-        for f in get_TSeries_folders(source_folder):
-            if os.path.isdir(os.path.join(f, 'suite2p', 'plane0')):
+        for f in [f for f in os.listdir(source_folder) if ('TSeries' in f)]:
+            if os.path.isdir(os.path.join(source_folder, f, 'suite2p', 'plane0')):
                 print('copying files from "%s"' % f)
-                new_folder = os.path.join(destination_folder, f.split(os.path.sep)[-1], 'suite2p', 'plane0')
+                new_folder = os.path.join(destination_folder, sf, f, 'suite2p', 'plane0')
                 pathlib.Path(new_folder).mkdir(parents=True, exist_ok=True)
+                print(new_folder)
                 for bfn in FILES:
-                    old = os.path.join(f, 'suite2p', 'plane0', bfn+'.npy')
+                    old = os.path.join(source_folder, f, 'suite2p', 'plane0', bfn+'.npy')
                     new = os.path.join(new_folder, bfn+'.npy')
                     shutil.copyfile(old, new)
-                old = os.path.join(f, f.split(os.path.sep)[-1]+'.xml')
-                new_folder = os.path.join(destination_folder, f.split(os.path.sep)[-1])
+                old = os.path.join(source_folder, f, f+'.xml')
+                new_folder = os.path.join(destination_folder, sf, f)
                 new = os.path.join(new_folder, f.split(os.path.sep)[-1]+'.xml')
                 shutil.copyfile(old,new)
         
