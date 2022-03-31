b75caf293f9f81d73f5a64e38d02ab7e5f747805
Yann Zerlaut <yann.zerlaut@icm-institute.org>
2022-03-31 16:03:35 +0200

    update realignement gain

--- physion/assembling/realign_from_photodiode.py
+++ physion/assembling/realign_from_photodiode.py
@@ -1,5 +1,5 @@
 import numpy as np
-import os
+import os, time
 from scipy.signal import argrelextrema
 from scipy.ndimage.filters import gaussian_filter1d
 
@@ -26,51 +26,60 @@ def realign_from_photodiode(signal,
     
     tlim, tnew = [0, t[-1]], 0
 
-    tstart, tshift = metadata['time_start'][0]-.5, 0
+    tstart, tshift = metadata['time_start'][0]-1, 0
     metadata['time_start_realigned'] = []
-    
+
+    if verbose:
+        print('smoothing photodiode signal [...]')
+
+    # smoothing the signal
+    smooth_signal = np.diff(gaussian_filter1d(np.cumsum(data), 20)) # integral + smooth + derivative
+    smooth_signal[:1000], smooth_signal[-10:] = smooth_signal[1000], smooth_signal[-1000] # to insure no problem at borders (of the derivative)
+
     # compute signal boundaries to evaluate threshold crossing of photodiode signal
-    H, bins = np.histogram(signal, bins=50)
+    H, bins = np.histogram(smooth_signal, bins=100)
     baseline = bins[np.argmax(H)+1]
-    high_level = np.max(signal)
+    threshold = (np.max(smooth_signal)-baseline)/2.
 
     # looping over episodes
     i=0
     while (i<len(metadata['time_duration'])) and (tstart<(t[-1]-metadata['time_duration'][i])):
-        cond = (t>=tstart+shift_time) & (t<=tstart+metadata['time_duration'][i]+max_time_delay) 
-        try:
-            tshift, integral, threshold = find_onset_time(t[cond]-tstart, signal[cond],
-                                                          smoothing_time=smoothing_time,
-                                                          baseline=baseline, high_level=high_level)
-            # print('ep.#%i, tshift=%.1f, prtcl_id=%i' % (i, tshift, metadata['protocol_id'][i]))
-            if debug and ((i>=istart_debug) and (i<istart_debug+n_vis)):
-                fig, ax = plt.subplots()
-                ax.plot(t[cond], integral, label='smoothed')
-                ax.plot(t[cond], integral*0+threshold, label='threshold')
-                ax.plot((tstart+tshift)*np.ones(2), ax.get_ylim(), 'k:', label='onset')
-                ax.plot((tstart+tshift+metadata['time_duration'][i])*np.ones(2), ax.get_ylim(), 'k:', label='offset')
-                ax.plot(t[cond], normalize_signal(signal[cond])[0]*.8*np.diff(ax.get_ylim())[0],
-                        label='photodiode-signal', lw=0.5, alpha=.3)
-                plt.xlabel('time (s)')
-                plt.ylabel('norm. signals')
-                ax.set_title('ep. #%i' % i)
-                ax.legend(frameon=False)
-                plt.show()
-        except BaseException as be:
-            print('\n', be)
-            print('\n'+' /!\ REALIGNEMENT FAILED (@ i=%i ) /!\ \n' % i)
-            # print(i, Nepisodes, metadata['time_duration'][i])
-            success = False # one exception is enough to make it fail
-        metadata['time_start_realigned'].append(tstart+tshift)
-        tstart=tstart+tshift+metadata['time_duration'][i] # update tstart by tshift_observed+duration
+        cond = (t[:-1]>=tstart+shift_time)
+        # the next time point above being above threshold
+        cond_thresh = (t[:-2]>=tstart+shift_time) & (smooth_signal[1:]>=(baseline+threshold)) & (smooth_signal[:-1]<(baseline+threshold))
+        if np.sum(cond_thresh)>0:
+            tshift = t[:-2][cond_thresh][0] - (tstart+shift_time) #
+            # success
+            metadata['time_start_realigned'].append(tstart+tshift)
+            tstart=tstart+tshift+metadata['time_duration'][i] # update tstart by tshift_observed+duration
+        elif verbose:
+            # we don't do anything, we just increment the episode id
+            print('episode #%i was not realigned' % i)
+        
+        if debug and ((i>=istart_debug) and (i<istart_debug+n_vis)):
+            cond = (t[:-1]>=tstart+shift_time-5) & (t[:-1]<=tstart+tshift+10)
+            fig, ax = plt.subplots()
+            ax.plot(t[:-1][cond], signal[:-1][cond], label='signal')
+            ax.plot(t[:-1][cond], smooth_signal[cond], label='smoothed')
+            ax.plot((tstart+tshift)*np.ones(2), ax.get_ylim(), 'k:', label='onset')
+            ax.plot(ax.get_xlim(), (baseline+threshold)*np.ones(2), 'k:', label='threshold')
+            ax.plot(ax.get_xlim(), baseline*np.ones(2), 'k:', label='baseline')
+            ax.plot((tstart+tshift+metadata['time_duration'][i])*np.ones(2), ax.get_ylim(), 'k:', label='offset')
+            plt.xlabel('time (s)')
+            plt.ylabel('norm. signals')
+            ax.set_title('ep. #%i' % i)
+            ax.legend(frameon=False)
+            plt.show()
+        
         i+=1
         
     if verbose:
-        if success:
-            print('[ok]          --> succesfully realigned')
-            print('                  found n=%i episodes over the %i of the protocol ' % (len(metadata['time_start_realigned']), len(metadata['time_start'])))
-        else:
-            print('[X]          --> realignement failed')
+        print('                  found n=%i episodes over the %i of the protocol ' % (len(metadata['time_start_realigned']), len(metadata['time_start'])))
+        # if success:
+        #     print('[ok]          --> succesfully realigned')
+        #     print('                  found n=%i episodes over the %i of the protocol ' % (len(metadata['time_start_realigned']), len(metadata['time_start'])))
+        # else:
+        #     print('[X]          --> realignement failed')
             
     if success:
         # transform to numpy array
@@ -88,20 +97,31 @@ def realign_from_photodiode(signal,
 
 
 def find_onset_time(t, photodiode_signal,
-                    smoothing_time = 20e-3,
-                    # advance_time = 15e-3,
                     baseline=0, high_level=1):
     """
-    we smooth the photodiode signal, with a gaussian filter of extent Tsmoothing
-    Tonset = Tcrossing-3./4.*Tsmoothing
-    Tcrossing is the time of crossing of half the max-min level (of the smoothed signal)
     """
-    advance_time = 3./4.*smoothing_time
-    smoothed = gaussian_filter1d(photodiode_signal, int(smoothing_time/(t[1]-t[0])))
-    smoothed = (smoothed-smoothed.min())/(smoothed.max()-smoothed.min())
-    cond = (smoothed[1:]>=0.5) & (smoothed[:-1]<=0.5)
-    t0 = t[:-1][cond][0]
-    return t0-advance_time, smoothed, smoothed.min()+0.5*(smoothed.max()-smoothed.min())
+    cond = (photodiode_signal[1:]>=(0.5*high_level)) & (photodiode_signal[:-1]<=(0.5*high_level))
+    if np.sum(cond)>0:
+        return t[:-1][cond][0]
+    else:
+        return None
+    
+
+# def find_onset_time(t, photodiode_signal,
+#                     smoothing_time = 20e-3,
+#                     # advance_time = 15e-3,
+#                     baseline=0, high_level=1):
+#     """
+#     we smooth the photodiode signal, with a gaussian filter of extent Tsmoothing
+#     Tonset = Tcrossing-3./4.*Tsmoothing
+#     Tcrossing is the time of crossing of half the max-min level (of the smoothed signal)
+#     """
+#     advance_time = 3./4.*smoothing_time
+#     smoothed = gaussian_filter1d(photodiode_signal, int(smoothing_time/(t[1]-t[0])))
+#     smoothed = (smoothed-smoothed.min())/(smoothed.max()-smoothed.min())
+#     cond = (smoothed[1:]>=0.5) & (smoothed[:-1]<=0.5)
+#     t0 = t[:-1][cond][0]
+#     return t0-advance_time, smoothed, smoothed.min()+0.5*(smoothed.max()-smoothed.min())
 
 
 def normalize_signal(x):
@@ -133,10 +153,10 @@ if __name__=='__main__':
     for key in VisualStim:
         metadata[key] = VisualStim[key]
 
-    plt.plot(data[::1000][:1000])
-    plt.title('photodiode-signal (subsampled/100)')
-    plt.show()
-    
+    # plt.plot(data[::1000][:1000])
+    # plt.title('photodiode-signal (subsampled/100)')
+    # plt.show()
+
     realign_from_photodiode(data, metadata,
                             debug=True,
                             istart_debug=args.istart_debug,
