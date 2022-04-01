359fa33983729e72b037c07bc5911fc2ef8af654
Yann Zerlaut <yann.zerlaut@icm-institute.org>
2022-04-01 16:25:24 +0200

    adding export to gif draft

---
+++ physion/dataviz/export.py
@@ -0,0 +1,11 @@
+"""
+Export sample of data to gif file
+"""
+
+import imageio
+
+with imageio.get_writer('/path/to/movie.gif', mode='I') as writer:
+    for filename in filenames:
+        image = imageio.imread(filename)
+        writer.append_data(image)
+        
