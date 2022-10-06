3885a4a7561964d187473d0512e243236211c5a5
Yann Zerlaut <yann.zerlaut@icm-institute.org>
2022-10-06 19:11:52 +0200

    startin gto add pdf export

--- physion/analysis/functional_maps.py
+++ physion/analysis/functional_maps.py
@@ -1,10 +1,39 @@
 import os, sys, pathlib
 import numpy as np
+from PIL import Image
 
 sys.path.append(str(pathlib.Path(__file__).resolve().parents[2]))
+
 from physion.intrinsic import Analysis
 from physion.dataviz.datavyz.datavyz import graph_env
-ge = graph_env('screen') # for display on screen
+
+ge = graph_env('manuscript') # for display on screen
+
+def build_pdf(datafolder):
+
+
+    width, height = int(8.27 * 300), int(11.7 * 300) # A4 at 300dpi
+
+    page = Image.new('RGB', (width, height), 'white')
+
+    maps = Analysis.load_maps(args.datafolder)
+
+    fig_alt = Analysis.plot_retinotopic_maps(maps, 'altitude')
+    fig_alt.savefig('/tmp/fig_alt.png', dpi=300)
+
+    fig_azi = Analysis.plot_retinotopic_maps(maps, 'azimuth')
+    fig_alt.savefig('/tmp/fig_azi.png', dpi=300)
+
+    start, space = int(1*300), int(1*300)
+
+    for name in ['alt', 'azi']:
+        fig = Image.open('/tmp/fig_%s.png'%name)
+        page.paste(fig, box=(space, start))
+        start+= fig.getbbox()[3]-fig.getbbox()[1] + 10
+        fig.close()
+
+    page.save('fig.pdf')
+
 
 if __name__=='__main__':
 
@@ -17,10 +46,5 @@ if __name__=='__main__':
     parser.add_argument('-v', "--verbose", action="store_true")
     
     args = parser.parse_args()
-    maps = Analysis.load_maps(args.datafolder)
 
-    Analysis.plot_retinotopic_maps(maps, 'altitude')
-    Analysis.plot_retinotopic_maps(maps, 'azimuth')
-
-    ge.show()
-
+    build_pdf(args.datafolder)
